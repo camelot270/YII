b84f45ffc628715545845edb654e893d16b798cd
Camelot270 <nick_1993ivt@mail.ru>
2017-12-10 20:04:36 +0300

    Php init  env = Development

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -44,6 +44,7 @@ AppAsset::register($this);
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
+        $menuItems[] = ['label' => 'Projects', 'url' => ['/projects/index']];
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
